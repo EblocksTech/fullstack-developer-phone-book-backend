cee36e6fe8dcd9835ef513da2c636e0f7d7ae0e4
lyleEblocks <lyle@eblocks.co.za>
2022-05-27 14:29:51 +0200

    [Lyle] Initial commit of api and api.test projects

---
+++ absa.phonebook.api.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace absa.phonebook.api.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ absa.phonebook.api/Controllers/PhonebookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace absa.phonebook.api.Controllers
+{
+    [ApiController]    
+    public class PhonebookController : ControllerBase
+    {        
+        public PhonebookController()
+        {
+        }
+
+        [HttpGet]
+        [Route("/phonebooks")]
+        public IActionResult Get()
+        {
+            return null;
+        }
+    }
+}
